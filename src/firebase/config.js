163e22b34ede8da3847dc09c86c70eecd65a5001
akap97 <rnkap1997@gmail.com>
2020-05-24 18:05:12 +0530

    v1

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export const config = {
+apiKey: "AIzaSyCWTHz8ksZgNYQOQ0b8tDuH-wOAucSpYSc",
+  authDomain: "confusionserver-18204.firebaseapp.com",
+  databaseURL: "https://confusionserver-18204.firebaseio.com",
+  projectId: "confusionserver-18204",
+  storageBucket: "confusionserver-18204.appspot.com",
+  messagingSenderId: "785132856263",
+  appId: "1:785132856263:web:3b70771d0d5354bdbf6fe2",
+  measurementId: "G-DRF6FCMWCE"
+};
